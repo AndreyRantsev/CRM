9cb1bda1bbf99367efc969a5b6cd3c78adb2fbb6
katzen <rantsev001@gmail.com>
2026-01-31 20:53:47 +0300

    add custom user and admin auth

--- schoolSystem/core/models.py
+++ schoolSystem/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ("admin", "админ"),
+        ("teacher", "учитель"),
+        ("parent", "родитель"),
+        ("student", "ученик")
+    )
+
+    roles = models.CharField(max_length=50, choices = ROLE_CHOICES, null=True)
+
+    def __str__(self):
+        return self.userName
+
