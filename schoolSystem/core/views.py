2668ac5451a7aba3aa169b51badca08e62c546d2
katzen <rantsev001@gmail.com>
2026-02-14 20:51:03 +0300

    started makeing middleware

--- schoolSystem/core/views.py
+++ schoolSystem/core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from email import message
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Teacher, Parent, Student, Message
 
 # Create your views here.
+
+@login_required
+def my_view(request):
+    if request.user.role == "teacher":
+        teacherData = Teacher.objects.get(request.user)
+        messages = Message.objects.filter() # Указать параметры для фильтрации
+        
+
+
+
