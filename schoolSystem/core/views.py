db36894b68b62c41c392c6de804889ae7b94f83c
katzen <rantsev001@gmail.com>
2026-03-01 18:51:24 +0300

    add message displaying for teacher

--- schoolSystem/core/views.py
+++ schoolSystem/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Teacher, Parent, Student, Message
+from .models import SchoolClass, Teacher, Parent, Student, Message
 
 # Create your views here.
 
@@ -8,12 +8,13 @@ from .models import Teacher, Parent, Student, Message
 def my_view(request):
     if request.user.roles == "teacher":
         teacherData = Teacher.objects.get(user = request.user)
-        # messages = Message.objects.filter() # Указать параметры для фильтрации
+        messages = Message.objects.filter(SchoolClass = teacherData.teacherClass)
         return render(
         request,
         "core/teacher.html",
         {
-            "school_class": teacherData.teacherClass
+            "school_class": teacherData.teacherClass,
+            "messages": messages
         }
         )
     return redirect("/")
